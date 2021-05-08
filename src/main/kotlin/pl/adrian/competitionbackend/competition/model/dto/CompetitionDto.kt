23b190ab1aa937d1c83a7d715b7862842e4310ac
Adam-Gajda <248877@student.pwr.edu.pl>
2021-05-08 17:06:49 +0200

    Add prediction data to statistics

--- src/main/kotlin/pl/adrian/competitionbackend/competition/model/dto/CompetitionDto.kt
+++ src/main/kotlin/pl/adrian/competitionbackend/competition/model/dto/CompetitionDto.kt
@@ -2,6 +2,7 @@ package pl.adrian.competitionbackend.competition.model.dto
 
 import pl.adrian.competitionbackend.competition.model.entity.Competition
 import pl.adrian.competitionbackend.user.model.dto.UserInfo
+import java.time.LocalDate
 import java.util.*
 import kotlin.streams.toList
 
@@ -12,7 +13,12 @@ class CompetitionDto(
         val category: String = "",
         val image: String = "",
         val addedById: String = "",
-        val usernames: List<UserInfo> = Collections.emptyList()
+        val bestAverageUserId: String = "",
+        val bestAverage: Double = 0.0,
+        val endDate: LocalDate? = null,
+        val usernames: List<UserInfo> = Collections.emptyList(),
+        val labels: Set<LocalDate> = Collections.emptySet(),
+        val labelsWholeCompetition: Set<LocalDate> = Collections.emptySet(),
 ) {
     companion object {
         fun toCompetitionDto(competition: Competition) = CompetitionDto(
@@ -22,7 +28,25 @@ class CompetitionDto(
                 category = competition.category,
                 image = competition.image,
                 addedById = competition.addedById,
+                bestAverageUserId = competition.bestAverageUserId,
+                bestAverage = competition.bestAverage,
+                endDate = competition.endDate,
                 usernames = competition.users
+        )
+
+        fun toCompetitionDtoWithLabels(competition: Competition, labels: Set<LocalDate>, labelsWholeCompetition: Set<LocalDate>) = CompetitionDto(
+                id = competition.id,
+                title = competition.title,
+                description = competition.description,
+                category = competition.category,
+                image = competition.image,
+                addedById = competition.addedById,
+                usernames = competition.users,
+                bestAverageUserId = competition.bestAverageUserId,
+                bestAverage = competition.bestAverage,
+                endDate = competition.endDate,
+                labels = labels,
+                labelsWholeCompetition = labelsWholeCompetition,
         )
     }
 }
---
+++ src/main/kotlin/pl/adrian/competitionbackend/competition/model/entity/Day.kt
@@ -0,0 +1,4 @@
+package pl.adrian.competitionbackend.competition.model.entity
+
+class Day(var amount: Int = 0,
+          var sum: Int = 0)
